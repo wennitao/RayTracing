6f8950af6ccb64a9d51af97d04c8424c0ac72ff5
wennitao <wennitao@126.com>
2021-08-11 15:48:26 +0800

    update

--- rtweekend.h
+++ rtweekend.h
@@ -23,6 +23,25 @@ inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
+#include <random>
+
+inline double random_double() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
+}
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
+
 // Common Headers
 
 #include "ray.h"
