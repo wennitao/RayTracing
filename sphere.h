084e673384c069eac829e76458930e17bb24ec17
wennitao <wennitao@126.com>
2021-08-11 17:49:49 +0800

    update

--- sphere.h
+++ sphere.h
@@ -7,14 +7,15 @@
 class sphere : public hittable {
     public:
         sphere() {}
-        sphere(point3 cen, double r) : center(cen), radius(r) {};
+        sphere(point3 cen, double r, shared_ptr<material> m) : center(cen), radius(r), mat_ptr(m) {};
 
         virtual bool hit(
             const ray& r, double t_min, double t_max, hit_record& rec) const override;
 
     public:
-        point3 center;
-        double radius;
+        point3 center ;
+        double radius ;
+        shared_ptr<material> mat_ptr ;
 };
 
 bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
@@ -38,7 +39,8 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
     rec.t = root;
     rec.p = r.at(rec.t);
     vec3 outward_normal = (rec.p - center) / radius;
-    rec.set_face_normal(r, outward_normal);
+    rec.set_face_normal(r, outward_normal) ;
+    rec.mat_ptr = mat_ptr ;
 
     return true;
 }
